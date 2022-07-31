60ea96ca44d78920dfdacdc72020cf5e2e3e4817
Ivan <ivansavichev884@gmail.com>
2022-07-31 21:18:07 +0300

    typing

---
+++ scripts/main.js
@@ -0,0 +1,122 @@
+let words = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ut quo harum nisi itaque perspiciatis modi ea incidunt porro recusandae mollitia, praesentium eveniet error magnam quidem! Esse obcaecati dolorem, consequuntur provident assumenda possimus iure ut cupiditate sunt eum nisi ad dolore adipisci ea! Ipsam quaerat optio reprehenderit vel blanditiis natus aliquam omnis, laboriosam porro illo obcaecati accusantium nulla cumque magnam quam nisi placeat esse modi fuga dicta explicabo? Cupiditate labore quam eos beatae error asperiores in quae nam deleniti itaque libero sapiente repellat unde, expedita officiis cumque quis impedit incidunt. Minima harum beatae tempora explicabo, enim quia eius ducimus consequatur iste culpa, nulla numquam nobis dignissimos in ad saepe ex ipsa architecto, veniam reprehenderit quis sunt aperiam laudantium! Reiciendis necessitatibus temporibus debitis iure repellat excepturi assumenda vel velit voluptatem quam quidem, error, placeat doloremque laudantium numquam veniam quisquam. Debitis voluptate perferendis odio rem consequuntur laboriosam ea, a totam adipisci exercitationem voluptates molestiae perspiciatis iusto explicabo commodi, quam veritatis molestias deleniti neque ullam dolor repellendus. Aliquid provident totam magnam a saepe animi necessitatibus error fugiat sit, debitis odit rerum officiis beatae dolorem ut eos, doloribus unde tenetur. Nihil, error natus possimus laboriosam similique incidunt sit ipsa saepe aliquid iusto beatae, est asperiores voluptates dolore tempore autem pariatur ea quo aperiam inventore fuga, sunt deserunt esse! Quam facilis nemo, dicta non esse repellat, placeat doloribus ratione dignissimos sed fugit consequatur voluptate reiciendis illum tempore molestias praesentium delectus eius ut? Dolorem rerum esse minus cupiditate ipsum. Labore perspiciatis dolor modi alias in. Veniam quidem vitae suscipit, laboriosam deserunt nam enim architecto, deleniti iste quaerat natus numquam incidunt. Nobis maxime, sunt animi nisi vitae ipsam libero explicabo! Reiciendis obcaecati, exercitationem ut quia dignissimos minus at quam praesentium qui ab officia accusantium temporibus deleniti quod dolore nostrum expedita fuga voluptas distinctio. Labore, quasi deleniti. Aliquam, enim unde. Nesciunt, cumque. Eveniet delectus placeat sed ex excepturi adipisci dolorem deserunt temporibus suscipit nesciunt? Dolorem error nulla quasi ipsam eius autem ipsa fugiat incidunt, cumque reprehenderit minus debitis saepe in fuga voluptatibus. Illo sapiente voluptatibus vel ipsa iste veritatis maxime minus culpa eaque labore, facere, laborum reprehenderit perferendis necessitatibus totam temporibus veniam possimus accusantium. Maiores quas est, sequi libero accusantium, aliquid quibusdam, ipsa deleniti iste porro quidem aperiam? Labore ipsa, repellendus impedit veritatis, fugiat nesciunt incidunt architecto, dolorem ipsam iure mollitia natus. Id adipisci officia consequatur, assumenda omnis aliquid non quisquam at natus animi accusamus magni quibusdam. Repellat ab rerum accusamus numquam. Recusandae minus sequi pariatur vero et sint reprehenderit adipisci totam error, aliquid officia temporibus cumque repellendus perspiciatis voluptas labore atque officiis ex. Totam deleniti, aliquid suscipit maxime quasi dolorem ipsam necessitatibus nisi expedita nobis excepturi qui quo! Obcaecati cumque quae eveniet vero veritatis, ab labore perferendis totam sit iusto tenetur repellat fugiat, dignissimos similique at debitis. Officiis minus ab amet deleniti dolorem velit, autem totam id tenetur deserunt quae ullam eos harum perferendis? Cum, error sint laborum laboriosam ex explicabo, saepe sunt modi iste perspiciatis officia voluptates similique commodi laudantium quis ducimus necessitatibus? Modi veritatis sunt exercitationem possimus ipsum consectetur, dignissimos libero, ea in deleniti magni totam aut velit. Voluptatum, assumenda tempore repellendus, quidem voluptatem ab in at sapiente dolorem adipisci rerum accusantium fuga incidunt maiores debitis suscipit officiis. Deleniti et eius placeat? Repellendus sint beatae libero id suscipit distinctio, enim ducimus ipsam expedita odio impedit reprehenderit illum asperiores numquam dolorum quisquam voluptates magnam. Magni rem ad nobis expedita voluptatibus accusamus eos ducimus ipsa sit, architecto nemo reprehenderit exercitationem! Possimus officia dolorum saepe quos id, ad mollitia totam, obcaecati officiis, doloremque nisi exercitationem quia. Architecto magnam atque nesciunt incidunt numquam cum, reiciendis laborum in autem animi et similique nostrum aspernatur non provident, aut commodi quisquam veritatis maxime quam, eaque tenetur tempore eligendi. Ab veniam magnam quas sit quod quis sequi enim rerum, libero perferendis in, soluta saepe aliquid fuga! Obcaecati doloribus beatae placeat commodi earum laudantium? Quod odio ad culpa est libero? Ducimus voluptate, maxime asperiores corporis enim repudiandae officiis suscipit aut tempore impedit quidem qui ipsam neque nemo deserunt fugiat! Dolore perferendis doloremque nisi facere eius impedit illum reprehenderit ea amet, ut, exercitationem dolor dicta. Animi cumque ad doloribus voluptas alias repellat! Nisi accusamus eaque, beatae nemo eius culpa porro similique, dicta deserunt possimus minus neque facilis eveniet aliquam voluptas molestiae est corporis! Ipsam incidunt similique perspiciatis distinctio temporibus tenetur odit at quam cum quia beatae rerum hic fugiat, minus mollitia ab pariatur. Veniam, exercitationem repudiandae? Doloribus ab repellendus, recusandae vel, hic mollitia sequi dolorum est error aliquid voluptates. Ratione, dolores. Optio blanditiis qui perferendis non exercitationem aliquam velit vero, fuga ipsam iste pariatur architecto fugit itaque inventore excepturi delectus enim ipsa doloremque dicta! Debitis saepe impedit eaque optio in at. Excepturi sint numquam recusandae aliquid maiores culpa delectus sapiente harum! Fugit id tenetur illum nihil, sunt eligendi veniam consequuntur ipsam dolorum dolore necessitatibus sit sequi ad vitae similique? Odit molestias corrupti itaque fugiat nobis velit sit, quo voluptate rerum nulla eum, ex dicta vero recusandae officia impedit earum consequatur amet optio accusantium cum nihil incidunt iste quos? Sunt, explicabo at! Accusantium deserunt pariatur nesciunt nisi quia nostrum at sint, sequi sapiente rerum quisquam quibusdam numquam culpa aspernatur, esse quae eaque, quaerat maiores atque officiis cum debitis? Optio doloribus est esse qui eligendi excepturi, libero error! Sit expedita dignissimos veniam nesciunt, pariatur nihil aliquid. Ullam consectetur quas eum unde sed, nobis quidem, sunt, sapiente voluptatem nemo perspiciatis blanditiis non animi ipsum molestiae voluptatum excepturi id possimus repellat! Nemo facere rem reiciendis magni veritatis vel cum officia ipsum eos animi sequi totam vero dicta doloremque aliquam id facilis non corporis ad quo, suscipit, dolorum harum quos vitae? Libero veritatis beatae excepturi itaque, nobis nulla ex iste eveniet non, possimus provident voluptatibus corrupti magnam neque tenetur asperiores fuga facere ab, debitis repellendus cumque cum eligendi sint. Et doloribus suscipit rem est, quisquam, cupiditate atque ex assumenda, officia provident commodi incidunt sit sequi! A, placeat, nisi voluptates odio illo itaque provident quos aut, pariatur quae velit eius vitae amet tempore officiis aperiam dolores veniam esse magni suscipit commodi.`
+words = words.toLowerCase()
+const elems = [`.`, `,`, `!`, `?`, `'`, `"`, `;`, `:`, `-`]
+const startCon = document.querySelector(".start-container")
+const btnStart = document.querySelector(".start-btn")
+const signs = document.querySelector('input[type="checkbox"]')
+const countWords = document.querySelector('input[type="number"]')
+const refresh = document.querySelector(".refresh")
+const queue = document.querySelector(".queue")
+const done = document.querySelector(".done")
+const error = document.querySelector(".errors")
+let timeStarted = false
+let errors
+let timer
+
+function deleteSigns(elem) {
+  return elem
+    .replace(/[.,\/#!$?%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s{2,}/g, " ")
+}
+
+btnStart.onclick = (event) => {
+  timeStarted = true
+  if (timeStarted == true) {
+    timerBegin()
+  }
+  errors = 0
+  error.textContent = `Errors: ${errors}`
+  capsChecker(event)
+  function newWords() {
+    let SeparatedWords = words.split(" ")
+    let newSentence = ""
+    for (let i = 0; i < +countWords.value; i++) {
+      newSentence +=
+        SeparatedWords[Math.floor(Math.random() * SeparatedWords.length)] + " "
+    }
+    return newSentence
+  }
+
+  let splitedWords = newWords()
+  refresh.style.fontSize = ""
+  document.querySelector(".text").style.display = ""
+  if (!signs.checked) {
+    splitedWords = deleteSigns(splitedWords)
+  }
+  splitedWords = splitedWords.split(" ")
+  splitedWords = splitedWords.join(" ").trim()
+  done.textContent = ""
+  queue.textContent = splitedWords
+  queue.focus()
+
+  document.addEventListener("keydown", typing)
+}
+
+refresh.onclick = () => {
+  if (refresh.classList.contains("refresh-start")) return
+  refresh.classList.add("refresh-start")
+  setTimeout(() => {
+    startCon.classList.remove("move-up")
+    refresh.classList.remove("refresh-start")
+    btnStart.focus()
+  }, 400)
+}
+
+function typing(event) {
+  event.preventDefault()
+  capsChecker(event)
+  if (event.key == queue.textContent[0]) {
+    done.textContent += queue.textContent[0]
+    queue.textContent = queue.textContent.slice(1)
+  } else {
+    if (event.key != "Shift" && event.key != "CapsLock") {
+      errors++
+      document.body.style.background = "#5a5d63"
+      error.textContent = `Errors: ${errors}`
+      setTimeout(() => {
+        document.body.style.background = ""
+      }, 250)
+    }
+  }
+  if (queue.textContent == "") {
+    clearInterval(timer)
+    timeStarted = false
+    document.removeEventListener("keydown", typing)
+    refresh.style.fontSize = 100 + "px"
+    document.querySelector(".text").style.display = "none"
+    refresh.focus()
+  }
+}
+
+function timerBegin() {
+  document.querySelector('.ss').textContent = '00'
+  document.querySelector('.mm').textContent = '00'
+  let seconds = 0
+  let minutes = 0
+  let secNode = document.querySelector('.ss')
+  let minNode = document.querySelector('.mm')
+  timer = setInterval(() => {
+    +seconds++
+    if (seconds == 60) {
+      +minutes++
+      seconds = 0
+    }
+    if (seconds < 10) {
+      secNode.textContent = "0" + seconds
+    } else secNode.textContent = seconds
+
+    if (minutes < 10) {
+      minNode.textContent = "0" + minutes
+    } else minNode.textContent = minutes
+  }, 1000)
+}
+
+function capsChecker(e) {
+  let caps = document.querySelector(".caps")
+  caps.textContent = "CapsLock on"
+  if (e.getModifierState && e.getModifierState("CapsLock")) {
+    caps.style.display = "block"
+  } else {
+    caps.style.display = ""
+  }
+}
---
+++ scripts/start.js
@@ -0,0 +1,42 @@
+function start() {
+  const startCon = document.createElement("div")
+  const btnStart = document.createElement("button")
+  const numberCount = document.createElement("input")
+  const paragraph = document.createElement("p")
+  const checkSigns = document.createElement("input")
+  const sighsLabel = document.createElement("label")
+
+  btnStart.className = "start-btn"
+  btnStart.textContent = "Начать"
+  btnStart.tabIndex = "-1"
+  startCon.appendChild(btnStart)
+  btnStart.addEventListener("click", () => {
+    startCon.classList.add("move-up")
+  })
+
+  paragraph.textContent = "Введите количество слов"
+  startCon.appendChild(paragraph)
+
+  numberCount.type = "number"
+  numberCount.value = "10"
+  numberCount.min = "1"
+  numberCount.max = "120"
+  startCon.appendChild(numberCount)
+  numberCount.addEventListener("input", () => {
+    if (numberCount.value > 120) numberCount.value = 120
+    if (numberCount.value <= -1) numberCount.value = 1
+  })
+
+  checkSigns.className = "signs"
+  checkSigns.type = "checkbox"
+  checkSigns.name = "signs"
+  sighsLabel.textContent = "Пунктуационные знаки"
+  startCon.appendChild(checkSigns)
+  startCon.appendChild(sighsLabel)
+
+  startCon.className = "start-container"
+  document.body.appendChild(startCon)
+  btnStart.focus()
+}
+
+start()
